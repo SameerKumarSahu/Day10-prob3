2f46edf4fef9c1ff6a14e9af11b50156c1eaeab1
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-06-27 18:23:23 +0530

    Doublet

--- CoinCombination.sh
+++ CoinCombination.sh
@@ -7,15 +7,26 @@ read -p  "Enter to Flip a coin"
 for ((i=1;i<=6;i++))
 do
 
-	x=$((RANDOM%2))
+	x=$((RANDOM%4))
 
 	if [ $x -eq 0 ]
 
 	then
-		Dict[face1]="Head"
+		Dict[face1]="HH"
+	elif [ $x -eq 1 ]
+	then
+                Dict[face2]="HT"
+	elif [ $x -eq 2 ]
+        then
+                Dict[face3]="TH"
 	else
-		 Dict[face2]="Tail"
+                Dict[face4]="TT"
+
 
 	fi
-done 
+done
+echo "${Dicr[@]}"
+
+
+	
 echo "${Dict[@]}"
