e130e73fa0c060602b1518b1999beaf980134f3c
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-06-27 18:17:53 +0530

    Singlet

--- CoinCombination.sh
+++ CoinCombination.sh
@@ -2,14 +2,20 @@
 #! /bin/bash -x
 
 echo "Welcome to the coin combination programm...."
-
+declare -A Dict
 read -p  "Enter to Flip a coin"
+for ((i=1;i<=6;i++))
+do
+
 	x=$((RANDOM%2))
 
 	if [ $x -eq 0 ]
 
 	then
-		echo "HEAD"
+		Dict[face1]="Head"
 	else
-		echo "TAIL"
+		 Dict[face2]="Tail"
+
 	fi
+done 
+echo "${Dict[@]}"
